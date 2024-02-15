f5953c7ff9f32dd95841a10b3995a507ddad5c71
Simphiwe Yende <syende023@student.wethinkcode.co.za>
2024-02-15 15:14:18 +0200

    main

--- src/main.py
+++ src/main.py
@@ -1,10 +1,8 @@
 import json
 import os.path
 import configure.configuration as configuration
-import calendars.calendars as calendars
-import calendars.calendar_api as calendar_interface
-from InquirerPy import inquirer
-
+import calendars.calendar_interface as calendar_interface
+import calendars.calendar_api as api
 
 TOKEN_FILE = os.path.expanduser("~/.google_calendar_token.json")
 CONFIG_FILE = os.path.expanduser("~/.coding_clinic_config.json")
@@ -25,18 +23,13 @@ def main():
     if not os.path.exists(CONFIG_FILE):
         configuration.first_run_setup()
 
-    service = calendar_interface.authorise_google_calendar(SCOPES,
-                                                           CREDS_FILE,
-                                                           TOKEN_FILE)
+    service = api.authorise_google_calendar(SCOPES,
+                                            CREDS_FILE,
+                                            TOKEN_FILE)
     try:
-        calendars.create_code_clinic_calendar(service)
+        clinic_calendar = calendar_interface.create_coding_clinic_calendar(service)
     except Exception as e:
         print(f"An error was encountered while creating calendar\n{e}")
-        try_again = inquirer.confirm(message="\nTry again?").execute()
-        if try_again:
-            main()
-
-    # TODO: Implement main loop logic here
 
     #     start_date = datetime(2024, 2, 13, 0, 0, 0)  
     #     end_date = datetime(2024, 2, 14, 0, 0, 0)    
