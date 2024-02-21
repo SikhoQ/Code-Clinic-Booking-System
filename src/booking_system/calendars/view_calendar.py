c20d3548a6f443b58d1962a01070e32ac2cbb007
Simphiwe Yende <syende023@student.wethinkcode.co.za>
2024-02-21 16:42:00 +0200

    added view calender module

---
+++ src/booking_system/calendars/view_calendar.py
@@ -0,0 +1,57 @@
+from datetime import *
+import calendar_interface
+import calendar_api as api
+import os
+import csv
+from prettytable import PrettyTable
+import calendar
+
+
+TOKEN_FILE = os.path.expanduser("~/.google_calendar_token.json")
+CONFIG_FILE = os.path.expanduser("~/.coding_clinic_config.json")
+CREDS_FILE = os.path.expanduser("~/.google_calendar_credentials.json")
+SCOPES = ["https://www.googleapis.com/auth/calendar"]
+
+
+
+def format_data(event):
+    start_time = datetime.fromisoformat(event['start']['dateTime']).strftime('%H:%M')
+    end_time = datetime.fromisoformat(event['end']['dateTime']).strftime('%H:%M')
+    summary = event['summary']
+        
+    return [summary ,start_time , end_time]
+
+
+
+def view_calendar():
+    service = api.authorise_google_calendar(SCOPES, CREDS_FILE, TOKEN_FILE)
+    today = datetime.now()
+    start_date = today.isoformat() + 'Z'
+    
+    end_date = (today + timedelta(days=6)).isoformat() + 'Z'
+    
+    table = PrettyTable()
+    table.field_names = ['Day', 'Date', 'Summary', 'Duration']
+
+    slots = []
+    calendar_data = calendar_interface.download_calendar_data(service, start_date, end_date)
+    
+    for event in calendar_data:
+    
+        formatted = format_data(event)
+        day = calendar.day_name[today.weekday()]
+        slots.append(today.isoformat() + 'Z')
+        today += timedelta(days=1)
+        table.add_row([day, (today - timedelta(days=1)).strftime("%d-%m-%Y"), formatted[0], f'{formatted[1]} - {formatted[2]}'])
+        table.align["Day"] = "l"
+        if not event:
+            formatted[0] = 'No event'
+
+
+    print(table)
+    
+    return slots
+
+    
+if __name__ == '__main__':
+    view_calendar()
